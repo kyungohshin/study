24bddef673a9a5c98622284f11b1fdf031732b99
kyungohshin <shin57027@gmail.com>
2021-03-15 23:53:51 +0900

    docker study

---
+++ myCoding/src/main/java/study/lambda/Main2.java
@@ -0,0 +1,14 @@
+package study.lambda;
+
+import java.util.function.UnaryOperator;
+
+/**
+ *
+ *
+ * @author USER
+ * @history
+ *          2021. 3. 14. initial creation
+ */
+public class Main2 {
+	UnaryOperator<Person> a;
+}
