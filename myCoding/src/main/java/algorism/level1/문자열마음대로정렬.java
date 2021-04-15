18375d9d143317bfb51156289047a3437f87e056
kyungohshin <shin57027@gmail.com>
2021-04-15 21:54:36 +0900

    알고리즘 : 문자열 마음대로 정렬

---
+++ myCoding/src/main/java/algorism/level1/문자열마음대로정렬.java
@@ -0,0 +1,38 @@
+package algorism.level1;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ *
+ *
+ * @author USER
+ * @history
+ *          2021. 3. 22. initial creation
+ */
+public class 문자열마음대로정렬 {
+
+	public static String[] solution(String[] strings, int n) {
+
+		Arrays.sort(strings, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				if (o1.charAt(n) > o2.charAt(n)) {
+					return 1;
+				} else if (o1.charAt(n) < o2.charAt(n)) {
+					return -1;
+				} else {
+					return o1.compareTo(o2);
+				}
+			}
+
+		});
+//		System.out.println(Arrays.toString(strings));
+
+		return strings;
+	}
+
+	public static void main(String args[]) {
+		solution(new String[] { "abce", "abcd", "cdx", "sun", "bed", "car" }, 1);
+	}
+}
