bf3d03812b8fc757d9f340c48680cc5f87e62323
kyungohshin <shin57027@gmail.com>
2021-06-01 20:12:45 +0900

    자바 병행성 : compute()

--- myCoding/src/main/java/study/concurrent/ConcurrentHashMap2.java
+++ myCoding/src/main/java/study/concurrent/ConcurrentHashMap2.java
@@ -12,22 +12,42 @@ import java.util.concurrent.atomic.AtomicLong;
  *          2021. 5. 30. initial creation
  */
 public class ConcurrentHashMap2 {
-	Map<String, AtomicLong> map = new ConcurrentHashMap<>();
+	Map<String, AtomicLong> atomicLongMap = new ConcurrentHashMap<>();
+	Map<String, Long> longMap = new ConcurrentHashMap<>();
 
-	public void mapTest() {
+	public void atomicLongMapTest() { // atomic 일때는 이런식으로 원자성을 지키면서 숫자증가
 		// map.get("ab")가 비어있으면 실행됨. 해당값을 넣고 map.get("ab")값이 리턴됨 (결국 넣은값).
-		AtomicLong d = map.computeIfAbsent("ab", x -> new AtomicLong(1));
+		AtomicLong d = atomicLongMap.computeIfAbsent("ab", x -> new AtomicLong(1));
 		Long l = d.incrementAndGet();
-		map.compute("ab", (x, y) -> y);
+	}
+
+	public void longMapTest() { // 기본 Long이나 Integer는 compute를 쓰면 원자성을 간편히 지킬수있음.
+		// compute 자체가 내부소스를 보면 cas알고리즘으로 원자성을 지키도록 구현되어있음. 결국 값을 AtomicLong으로 굳이 안바꿔도됨.
+		Long d = longMap.compute("ab", (key, val) -> val == null ? 1 : val + 1);
+	}
+
+	public void longMapAbsentTest() {
+		// compute 인데 map.get("")한값이 없을때만 실행됨
+		longMap.computeIfAbsent("ab", x -> 1L);
+	}
+
+	public void longMapPresentTest() {
+		// compute 인데 map.get("")한값이 있을때만 실행됨
+		longMap.computeIfPresent("ab", (key, val) -> val + 1);
+	}
+
+	public void longMapMergeTest() {
+		// 키가 처음 추가될때도 뭔가 특별한작업을 해줘야할때 사용 = computeIfAbsent + computeIfPresent
+		longMap.merge("ab", 1L, (x, y) -> x + y);
 	}
 
 	public void print() {
-		System.out.println(map.get("ab"));
+		System.out.println(atomicLongMap.get("ab"));
 	}
 
 	public static void main(String[] args) {
 		ConcurrentHashMap2 map = new ConcurrentHashMap2();
-		map.mapTest();
+		map.atomicLongMapTest();
 		map.print();
 	}
 
