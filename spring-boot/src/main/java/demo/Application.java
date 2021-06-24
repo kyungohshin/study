c702e46964b1882ee6b3d6d1a84830e884b8b49d
kyungohshin <shin57027@gmail.com>
2021-06-24 18:20:02 +0900

    스프링부트 : profile 수정

--- spring-boot/src/main/java/demo/Application.java
+++ spring-boot/src/main/java/demo/Application.java
@@ -1,15 +1,27 @@
 package demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import demo.domain.Config;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+	@Autowired
+	Config config;
 
 	public static void main(String[] args) {
 //		SpringApplication springApplication = new SpringApplication(Application.class);
 //		springApplication.setWebApplicationType(WebApplicationType.NONE);
 //		springApplication.run(args);
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(config.getId());
 	}
 }
